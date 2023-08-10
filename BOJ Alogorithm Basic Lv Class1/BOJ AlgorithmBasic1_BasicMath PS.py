eb39fd4f0227534fffd5d967ac968eef237ccdeb
ikjoo5 <137624266+ikjoo5@users.noreply.github.com>
2023-08-11 00:55:50 +0900

    Update BOJ AlgorithmBasic1_BasicMath PS.py

--- BOJ Alogorithm Basic Lv Class1/BOJ AlgorithmBasic1_BasicMath PS.py
+++ BOJ Alogorithm Basic Lv Class1/BOJ AlgorithmBasic1_BasicMath PS.py
@@ -77,7 +77,87 @@ def gcd(x,y):
     else:
         return gcd(y, x % y)
 print(gcd(a,b), int(a*b/gcd(a,b)), sep = '\n')
+################################## 2023-08-10 #########################################
 
+    ### 3: 소수 (Prime Number)
+
+        ### 주어진 수 x 가 소수인지 아닌지 판별하는법
+
+            # 1-1
+                # 그냥 2부터 x-1까지 하나씩 for문을 통해 나누어보면된다
+            
+            # 1-2
+                # 다만 좀더 효율적인 방법으로는 2 부터 x/2를 실행하면 된다(시간 비슷)
+'''
+            # 1-3 (제일빠른방법이다)
+                # 2부터 루트x까지 탐색해도 소수판별가능
+                # i ==2 to i*i <= x 
+import math
+def is_prime_number(x):
+    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        # x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+print(is_prime_number(4)) # 4는 소수가 아님
+print(is_prime_number(7)) # 7은 소수임
+
+
+        ### N이하의 수중에 소수를 구하는 방법
+            ####### 에라토스테네스의 체(Sieve of Eratosthenes) #########
+                # 소수를 구할 때 제일 좋은 알고리즘
+
+def prime_list(n):
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * n
+
+    m = int(n ** 0.5)   # 루트 of n b/c 루트n으로 탐색하는게 시간이 더 빠름
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우 
+            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]     # 아래 for문을 요약 
+
+    # for i in range(2,n):
+    #     if sieve[i] == True
+    #     return i
+
+
+            ### Practice BOJ 1929번 소수 구하기
+# 내 풀이(오답) 접근을 달리해보자
+m , n = map(int, input().split())
+
+sieve = [True] * n
+
+for i in range(m,n+1):
+    if sieve[i] == True:
+        for j in range(i+i, n, i):
+            sieve[j] = False
+
+for i in range(m,n+1):
+    if sieve[i] == True:
+        print(i)
+
+# 모범답안
+    # 기존에 에라토스원리는 1부터 n까지
+    # 따라서 n까지 구하고 다시for문으로 m,n으로 설정
+M, N = map(int, input().split())
+
+N += 1
+sieve = [True] * N
+for i in range(2, int(N**0.5)+1):
+    if sieve[i]:
+        for j in range(2*i, N, i):
+            sieve[j] = False
+for i in range(M, N):
+    if i > 1:
+        if sieve[i]:
+            print(i)
+'''
 
 
 
